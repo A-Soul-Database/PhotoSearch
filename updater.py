7e2a451df43c991bf50987d66fe14f165434b079
ppzzhh1186861238 <ppzzhh1186861238@hotmail.com>
2022-02-03 16:49:35 +0800

    fix

--- updater.py
+++ updater.py
@@ -6,7 +6,7 @@ import main
 import os
 import time
 from zipfile import ZipFile
-
+header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"}
 Remote_Indexer = []
 for i in requests.get("https://raw.githubusercontent.com/A-Soul-Database/A-Soul-Data/main/db/main.json").json()["LiveClip"]:
     Remote_Indexer+=requests.get(f"https://raw.githubusercontent.com/A-Soul-Database/A-Soul-Data/main/db/{i}/indexer.json").json()
@@ -16,7 +16,12 @@ Saved_Indexer = json.loads(open("indexer.json","r").read())
 Need_To_Update = [fn for fn in Remote_Indexer if fn not in Saved_Indexer]
 
 # Download Latest Released Aplhas
-Latest_Release =  requests.get("https://api.github.com/repos/A-Soul-Database/PhotoSearch/releases/latest").json()["assets"][0]["browser_download_url"]
+Latest_Release =  requests.get("https://api.github.com/repos/A-Soul-Database/PhotoSearch/releases/latest",headers=header).json()
+try:
+    Latest_Release = Latest_Release["assets"][0]["browser_download_url"]
+except:
+    print(Latest_Release)
+
 with closing(requests.get(Latest_Release)) as r:
     chunk_size = 10240
     with open("Alphas.zip","wb") as f:
@@ -25,21 +30,22 @@ with closing(requests.get(Latest_Release)) as r:
 
 Alphas = ZipFile("Alphas.zip")
 Alphas.extractall("./")
-os.remove("Alphas.zip")
 
 # Update Indexer
 
 def getPs(bv):
-    header = {"User-Agent":"Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36"}
     r = requests.get("https://api.bilibili.com/x/web-interface/view?bvid="+bv,headers=header).json()
+    if r["code"] = -404:
+        return []
     return [fn+1 for fn in range(len(r["data"]["pages"])) if "弹幕" not in r["data"]["pages"][fn]["part"]]
 
 for item in Need_To_Update:
-    for ps in getPs(item):
-        name = item if len(getPs(item)) == 1 else f"{item}-{ps}"
+    items = getPs(item)
+    for ps in items:
+        name = item if len(items) == 1 else f"{item}-{ps}"
         Video = os.system(f"you-get -O ./{name} --format=dash-flv360 https://www.bilibili.com/video/{item}?p={ps}")
 
-main.HashListGen().CaucalateAll()
+#main.HashListGen().CaucalateAll()
 
 # Create Release
 
@@ -50,6 +56,7 @@ with open(env_file, "a") as f:
     f.write("\n")
     f.write(f"Tags={times}")
 
+os.remove("Alphas.zip")
 with ZipFile("Alphas.zip","w") as zip:
     for i in os.walk("./Alphas"):
         for j in i[2]:
